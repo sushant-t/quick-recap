730e7bdca0913475505a2e21a215c5a8fb431e8f
Sushant Thyagaraj <sushantt@gmail.com>
2023-04-13 13:00:27 -0700

    Modularized the Whisper script

--- backend/whisper/decode.py
+++ backend/whisper/decode.py
@@ -1,3 +1,4 @@
+from numpy import ndarray
 import whisper
 import torch
 
@@ -7,19 +8,26 @@ import argparse
 
 os.chdir(sys.path[0])
 
-parser = argparse.ArgumentParser(description='Whisper')
-parser.add_argument('--model_size', action="store", dest="model_size", default="tiny")
-args = parser.parse_args()
+def get_audio_duration(audio: ndarray): # gives us audio duration from 16 kHz waveform in seconds
+    return len(audio)/16000
 
-model = whisper.load_model(args.model_size)
+def main():
+    parser = argparse.ArgumentParser(description='Whisper')
+    parser.add_argument('--model_size', action="store", dest="model_size", default="tiny")
+    args = parser.parse_args()
 
-audio = whisper.load_audio("../../video.mp3")
-trimmed_audio = whisper.pad_or_trim(audio)
-mel = whisper.log_mel_spectrogram(trimmed_audio).to(model.device)
+    model = whisper.load_model(args.model_size)
 
-_, probs = model.detect_language(mel)
-lang = max(probs, key=probs.get)
+    audio = whisper.load_audio("../../video.mp3")
 
-result = model.transcribe(audio=audio,fp16=torch.cuda.is_available(),language=lang,without_timestamps=True,verbose=True)
+    print("backend: audio_duration={0}".format(get_audio_duration(audio)))
+    trimmed_audio = whisper.pad_or_trim(audio)
+    mel = whisper.log_mel_spectrogram(trimmed_audio).to(model.device)
+
+    _, probs = model.detect_language(mel)
+    lang = max(probs, key=probs.get)
+
+    model.transcribe(audio=audio,fp16=torch.cuda.is_available(),language=lang,without_timestamps=True,verbose=True)
 
 
+main()
